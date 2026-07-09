817ccb2ebb0cc1ff31b2c70598db80bab51396bc
sprinklezai <sprinklezai@gmail.com>
2026-07-09 21:40:13 +0400

    Manual Push to Test

--- backend/services/fileStorageService.js
+++ backend/services/fileStorageService.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const SALES_PATH =
     process.env.SALES_DATA_PATH ||
-    "/home/u757775108/domains/sprinkleztrading.com/public_html/nodejs/backend/data/sales/monthly/";
+    "/home/u757775108/domains/sprinkleztrading.com/public_html/sales-data/monthly";
 
 async function loadSalesZip(month = "2026_06") {
 
